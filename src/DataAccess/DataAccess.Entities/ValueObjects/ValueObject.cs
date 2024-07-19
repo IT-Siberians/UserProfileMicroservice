6063d68a76df5babb9cf63e5fcb810f9a2b10e09
constantin <constantin.prog@gmail.com>
2024-07-19 14:08:07 +0700

    #GD-66 state: to verify work 1h implement the Equatable interface for the ValueObject<T> class

--- src/DataAccess/DataAccess.Entities/ValueObjects/ValueObject.cs
+++ src/DataAccess/DataAccess.Entities/ValueObjects/ValueObject.cs
@@ -3,18 +3,58 @@ using DataAccess.Entities.ValueObjects.Validation;
 
 namespace DataAccess.Entities.ValueObjects;
 
-public abstract class ValueObject<T>
+public abstract class ValueObject<T> : IEquatable<ValueObject<T>>
 {
     private readonly IValidator<T> _validator;
-    public T Value { get; }
+    public readonly T Value;
 
-    public ValueObject(IValidator<T> validator, T value)
+    protected ValueObject(IValidator<T> validator, T value)
     {
         _validator = validator;
         if (_validator == null)
             throw new ValidatorNotSpecifiedException(this);
         if (!_validator.Validate(value))
             throw _validator.GetValidationException();
+        if (value == null)
+            throw new ArgumentNullException($"{nameof(value)} cannot be null");
         Value = value;
+    }
+
+    public override string ToString()
+    {
+        return Value?.ToString() ?? GetType().ToString();
+            }
+
+    public override int GetHashCode()
+    {
+        return Value!.GetHashCode();
+    }
+
+    public override bool Equals(object? other)
+        => Equals(other as ValueObject<T>);
+
+    public bool Equals(ValueObject<T>? other)
+    {
+        if (ReferenceEquals(this, other))
+            return true;
+        if (other == null)
+            return false;
+        if (GetType() != other.GetType())
+            return false;
+        return other.Value!.Equals(Value);
+    }
+
+    public static bool operator ==(ValueObject<T>? left, ValueObject<T>? right)
+    {
+        if (((object?)left) == null || ((object?)right) == null)
+            return Object.Equals(left, right);
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(ValueObject<T>?    left, ValueObject<T>? right)
+    {
+        if (((object?)left) == null || ((object?)right) == null)
+            return !Object.Equals(left, right);
+        return !left.Equals(right);
     }
 }
